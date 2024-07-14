d2a728787e04a89642ae4857fce0623619d983da
Lisitha Dissanayaka <114801465+LisithaDiz@users.noreply.github.com>
2024-07-15 05:14:10 +0530

    Update Knuth_Morris_Pratt.scala

--- String Matching/Knuth_Morris_Pratt.scala
+++ String Matching/Knuth_Morris_Pratt.scala
@@ -1,55 +1,55 @@
-// function kmp_search(text, pattern):
-//     n = length of text
-//     m = length of pattern
-//     pi = compute_prefix_function(pattern)
-//     q = 0
-//     for i = 0 to n - 1:
-//         while q > 0 and pattern[q] != text[i]:
-//             q = pi[q - 1]
-//         if pattern[q] == text[i]:
-//             q = q + 1
-//         if q == m:
-//             print("Pattern found at index", i - m + 1)
-//             q = pi[q - 1]
-
-// function compute_prefix_function(pattern):
-//     m = length of pattern
-//     pi = array of length m
-//     k = 0
-//     for i = 1 to m - 1:
-//         while k > 0 and pattern[k] != pattern[i]:
-//             k = pi[k - 1]
-//         if pattern[k] == pattern[i]:
-//             k = k + 1
-//         pi[i] = k
-//     return pi
-
-
 object KMP{
-    def(String : text, String: pattern): Unit = {
+
+    def kmp_search(text: String, pattern: String): Unit = {
+        var m = pattern.length() 
         var n = text.length()
-        var m = pattern.length()
+        var pi = create_pi(pattern)
+        var q = 0
 
+        for (i <- 0 to n - 1){
+            while((q>0) && pattern(q) != text(i)){
+                printf(s" Compare ${pattern (q)} != ${text(i)} (${i}) ")
+                q = pi(q-1)
+            }
 
+            if(pattern (q) == text(i)){
+                printf(s" Compare ${pattern (q)} == ${text(i)} (${i}) \n")
+                q = q + 1
+            }
 
+            if(q == m){
+                println(s"Pattern found at index - ${i - m + 1}")
+                q = pi(q-1)
+            }
+        }
     }
 
-    def compute_prefix_function(String: pattern): Array[Char]{
+    def create_pi(pattern: String): Array[Int] = { // ababd
         var m = pattern.length() // 5
-        var pi = new Array[Char](m) // _ _ _ _ _
+        var pi = new Array[Int](m) // 0, 0, 0, 0, 0
         var k = 0
-        for (i <- 1 to (m-1)){
+        for (i <- 1 to m-1){
+
             while((k > 0) && (pattern(k) != pattern(i))){
                 k = pi(k - 1)
             }
+
             if(pattern(k) == pattern(i)){
                 k = k + 1
             }
+
             pi(i) = k
-        }   
+        }
         pi
     }
 
+    def main(args: Array[String]): Unit = {
+        val pi = create_pi("ababd")
+        println(pi.mkString(", "))
+
+        kmp_search("ababcabcabababd", "ababd")
+        
+    }
     
 
 
